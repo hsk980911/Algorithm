a415aea370f2813cfcd9c1f2e8f2de3ce8330e9b
soon9 <81350489+hsk980911@users.noreply.github.com>
2022-11-04 18:05:50 +0900

    221104

--- 구현/2167_bj.py
+++ 구현/2167_bj.py
@@ -2,27 +2,16 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 arr = []
-for _ in range(N):
-    # arr.append(list(map(int, sys.stdin.readline().split())))
-    arr.extend(list(map(int, sys.stdin.readline().split())))
-    
-k = int(sys.stdin.readline())
 
+for _ in range(N):
+    arr.append(list(map(int, sys.stdin.readline().split())))
+    
+dp = [[0]*(M+1) for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] =arr[i-1][j-1] - dp[i-1][j-1] + dp[i][j-1] + dp[i-1][j]
+
+k = int(sys.stdin.readline())
 for _ in range(k):
     i, j, x, y = map(int, sys.stdin.readline().split())
-    if i == x and j == y:
-        print(arr[(i-1)*M + (j-1)])
-    elif i == x:
-        print(sum(arr[(i-1)*M + (j-1) : (i-1)*M + (j-1) + (y-j+1)]))
-    elif j == y:
-        s = 0
-        for rows in range(i, x+1):
-            s += arr[(rows-1)*M +(j-1)]
-        print(s)    
-    else:
-        print(sum(arr[(i-1)*M + (j-1):(x-1)*M + (y-1)+1]))
-    # tmp = []
-    # for row in range(i-1, x):
-    #     tmp.append(arr[row][j-1:y])
-    #     tmp2 = sum(tmp, [])
-    #     print(sum(tmp2))
+    print(dp[x][y] - dp[i-1][y] - dp[x][j-1] + dp[i-1][j-1])
