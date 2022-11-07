e703fa9384ed9bfb8428220c1cbcf538285c40bc
soon9 <81350489+hsk980911@users.noreply.github.com>
2022-11-07 18:17:59 +0900

    221107

---
+++ 코딩마스터스/4322)cm.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(sys.stdin.readline())
+scores = [tuple(sys.stdin.readline().split()) for _ in range(N)]
+
+scores = sorted(scores, key=lambda x:x[0])
+scores = sorted(scores, key=lambda x:x[1])
+
+for i in scores:
+    print(i[0], end = ' ')
