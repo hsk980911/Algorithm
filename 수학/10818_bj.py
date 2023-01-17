40456da93548958a023919eb1e40911db145d5e2
Hwang Sun Gyu <hsk980911@gmail.com>
2023-01-17 15:28:56 +0900

    230117

--- 수학/10818_bj.py
+++ 수학/10818_bj.py
@@ -1,5 +1,4 @@
 import sys
 
 N = int(sys.stdin.readline())
-nums = list(map(int, sys.stdin.readline().split()))
-print(min(nums), max(nums))
+print(sum(list(map(int, sys.stdin.readline().strip()))))
