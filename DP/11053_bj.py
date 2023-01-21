6a13dded8ed50855a0ac781313c04e75558d7c2c
Hwang Sun Gyu <hsk980911@gmail.com>
2023-01-21 14:40:15 +0900

    230121

--- DP/11053_bj.py
+++ DP/11053_bj.py
@@ -2,11 +2,10 @@ import sys
 
 N = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
-dp = [0] * len(arr)
+dp = [1] * len(arr)
 
 for i in range(len(arr)):
     for j in range(i):
-        if dp[i] < dp[j] and arr[i] > arr[j]:
-            dp[i] = dp[j]
-    dp[i] += 1
-print(dp)
+        if dp[i] <= dp[j] and arr[i] > arr[j]:
+            dp[i] = dp[j]+1
+print(max(dp))
